72fb6a3836d8764956011f9e03450b6057ba39cf
Francis Onyach <Franblog7@gmail.com>
2023-11-20 18:26:10 +0300

    Created models

---
+++ backend/Kuizdoc_app/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.contrib.auth.models import User as UserAuth
+
+class Documents(models.Model):
+    DocumentID = models.AutoField(primary_key=True)
+    UserID = models.ForeignKey(UserAuth, on_delete=models.CASCADE)
+    DocumentText = models.TextField()
+    DateCreated = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Documents"
+
+class QuizQuestions(models.Model):
+    QuestionID = models.AutoField(primary_key=True)
+    DocumentID = models.ForeignKey(Documents, on_delete=models.CASCADE)
+    QuestionText = models.TextField()
+    OptionA = models.CharField(max_length=200)
+    OptionB = models.CharField(max_length=200)
+    OptionC = models.CharField(max_length=200)
+    OptionD = models.CharField(max_length=200)
+    CorrectAnswer = models.CharField(max_length=1)  # Assuming correct answer is a single character (A, B, C, or D)
+    DateCreated = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "QuizQuestions"   
+
+class UserAnswers(models.Model):
+    AnswerID = models.AutoField(primary_key=True)
+    UserID = models.ForeignKey(UserAuth, on_delete=models.CASCADE)
+    QuestionID = models.ForeignKey(QuizQuestions, on_delete=models.CASCADE)
+    UserAnswer = models.CharField(max_length=1)  # Assuming user's answer is a single character (A, B, C, or D)
+    DateCreated = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "UserAnswers"
+
+class UserScores(models.Model):
+    ScoreID = models.AutoField(primary_key=True)
+    UserID = models.ForeignKey(UserAuth, on_delete=models.CASCADE)
+    QuizID = models.ForeignKey(QuizQuestions, on_delete=models.CASCADE)
+    Score = models.IntegerField()
+    DateCreated = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "UserScores"
+
